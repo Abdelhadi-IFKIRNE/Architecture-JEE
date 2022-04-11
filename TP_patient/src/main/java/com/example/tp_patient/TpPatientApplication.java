72a2457b7ad91de87a1244a883c615c6cfdc2888
abdelhadi-IFKIRNE <abdelhadi.ifkirne@gmail.com>
2022-04-11 11:27:32 +0200

    TP-controle

---
+++ TP_patient/src/main/java/com/example/tp_patient/TpPatientApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp_patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpPatientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpPatientApplication.class, args);
+    }
+
+}
